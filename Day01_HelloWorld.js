284068a903324398aa26fb0659a2bd062b7d6f70
Cuneyt <june8kc@gmail.com>
2021-08-30 13:50:49 +0300

    visual studio Java Script ilk gonderme

---
+++ Day01_HelloWorld.js
@@ -0,0 +1,8 @@
+//Create a class: Day01_HelloWorld.js
+//how to run js code on IDE?
+// we need node JS to run javascript outside of the browser
+//node CLASSNAME
+//E.G. node Day01_HelloWorld.js
+console.log('Hello World !');
+
+console.log(10+7);
---
+++ Day01_Variables.js
@@ -0,0 +1,28 @@
+var myName
+console.log(myName);// by default, variables are undefined
+
+// var varibles can be used multiple times
+var myName='Javascript';
+console.log(myName);//"Javascript"
+
+myName='Java';
+console.log(myName);//"Java"
+
+myName=true;
+console.log(myName);//true
+
+myName=6;
+console.log(myName);//6
+
+//var fName;
+//var lName;
+
+//var fName, lName;
+
+var fName='John', lName='Cash';
+console.log(fName);//"John"
+console.log(lName);
+
+var firstName='Sam';
+var lastName='Walton';
+console.log(firstName+ ' '+lastName);
